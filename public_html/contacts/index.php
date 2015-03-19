8ce4c6c25f1ccaa3beb9c2de5a4d86ceceda0f74
dayAlone <andrey.slider@gmail.com>
2015-03-19 22:55:18 +0500

    Images

--- public_html/contacts/index.php
+++ public_html/contacts/index.php
@@ -5,7 +5,7 @@ $APPLICATION->SetTitle('Контакты');
 //$APPLICATION->SetPageProperty('show_courses', 'true');
 $APPLICATION->SetPageProperty('body_class', "contacts");?>
 <div class="row">
-  <div class="col-md-8">
+  <div class="col-lg-8">
 	  <p><big><nobr><strong>Адрес: </strong>119048, Россия, г. Москва, ул. Усачева, д. 35 стр. 1</nobr><br>
 <strong>Телефон: </strong><a href="tel:84956265359">8 (495) 626 53 59</a><br>
 <nobr><strong>Директор: </strong>Литвинова Татьяна Александровна - <a href="tel:89160941147">+7 (916) 094 11 47</a></nobr><br>
@@ -14,7 +14,7 @@ $APPLICATION->SetPageProperty('body_class', "contacts");?>
 <strong>E-mail: </strong><a href="mailto:tlitvinova@uic-spektr.ru">tlitvinova@uic-spektr.ru</a><br>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="mailto:atyutyunnik@mnpo-spektr.ru">atyutyunnik@mnpo-spektr.ru</a></big><br></p>
   </div>
-  <div class="col-md-4">
+  <div class="col-lg-4">
     <div class="xs-left md-right"><a data-toggle="modal" data-target="#Feedback" href="#Feedback" class="no-margin-top button button--big">напишите нам</a></div>
   </div>
 </div>
